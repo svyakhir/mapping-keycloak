afa4aa225ce945f4eae041600eab31385d84be3c
svyakhir <sergeivyahirev@gmail.com>
2024-09-28 16:29:57 +0300

    add function get_token_keycloak()

--- get_token_keycloak.py
+++ get_token_keycloak.py
@@ -5,30 +5,33 @@ from config_keycloak import *
 keycloak_url = f"{keycloak_url_m}/realms/{realm_name_m}/protocol/openid-connect/token"
 
 # Получение токена
-data = {
-    "client_id": client_id_m,
-    "client_secret": client_secret_m,
-    "username": username_m,
-    "password": password_m,
-    "grant_type": "password"
-}
+def get_token_keycloak():
+    data = {
+        "client_id": client_id_m,
+        "username": username_m,
+        "password": password_m,
+        "grant_type": "password"
+    }
 
-# Отправка POST-запроса для получения токена
-response = requests.post(keycloak_url, data=data)
+    # Отправка POST-запроса для получения токена
+    response = requests.post(keycloak_url, data=data)
 
-# Вывод отладочной информации
-# print("Request URL:", response.request.url)
-# print("Request Headers:", response.request.headers)
-# print("Request Body:", response.request.body)
-# print("Status Code:", response.status_code)
-# print("Response Text:", response.text)
+    # Вывод отладочной информации
+    # print("Request URL:", response.request.url)
+    # print("Request Headers:", response.request.headers)
+    # print("Request Body:", response.request.body)
+    # print("Status Code:", response.status_code)
+    # print("Response Text:", response.text)
 
-# Проверка успешности запроса
-if response.status_code == 200:
-    token_info = response.json()  # Получаем данные в формате JSON
-    access_token = token_info.get('access_token')  # Извлекаем access_token
-    print(f"{access_token}")
-    # print(token_info)
-else:
-    print(f"Ошибка получения токена: {response.status_code}")
-    print(response.text)
+    # Проверка успешности запроса
+    if response.status_code == 200:
+        token_info = response.json()  # Получаем данные в формате JSON
+        access_token = token_info.get('access_token')  # Извлекаем access_token
+        return access_token
+        # print(f"{access_token}")
+        # print(token_info)
+    else:
+        return  f"Ошибка получения токена: {response.status_code}. Для дополнительной информации испльзуйте вывод " \
+                f"отладочной информации"
+
+print(get_token_keycloak())
